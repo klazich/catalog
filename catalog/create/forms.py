1ae24d6d71d70d7249db6d3c92deb9d7b7b2e441
Kevin Lazich <dev@kevinlazich.com>
2018-01-03 15:14:38 -0500

    Roll-up of work

--- catalog/create/forms.py
+++ catalog/create/forms.py
@@ -1,10 +1,41 @@
-from flask_wtf import Form
-from wtforms import TextAreaField, StringField, SubmitField, SelectField
-from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, StringField, SubmitField, RadioField, SelectField
+from wtforms.validators import InputRequired
+
+from catalog.database import Session
+from catalog.models import Category
 
 
-class CreateItemForm(Form):
-    category = StringField('category', validators=[DataRequired()])
-    name = StringField('name', validators=[DataRequired()])
-    description = TextAreaField(label='description', validators=[DataRequired()])
-    submit = SubmitField("Send")
+class CreateCategoryForm(FlaskForm):
+    categories = SelectField(
+        'category name',
+        choices=[(c.name, c.name) for c in Session.query(Category).all()],
+        render_kw={'placeholder': 'choose a category'})
+
+    new_category = StringField(
+        'category name',
+        render_kw={'placeholder': 'enter category name'})
+
+    category_submit = SubmitField()
+
+
+class CreateItemForm(CreateCategoryForm):
+    # new_category = StringField(
+    #     'create new category',
+    #     render_kw={'placeholder': 'Create New Category'})
+    # categories = SelectField(
+    #     'choose category',
+    #     choices=[(c.name, c.name) for c in Session.query(Category).all()] + [('new_category', 'new category')],
+    #     render_kw={"placeholder": "Category Name"})
+
+    name = StringField(
+        'item name',
+        validators=[InputRequired()],
+        render_kw={'placeholder': 'enter item name'})
+
+    description = TextAreaField(
+        'item description',
+        validators=[InputRequired()],
+        render_kw={'placeholder': 'Enter item description'})
+
+    item_submit = SubmitField()
--- catalog/create/views.py
+++ catalog/create/views.py
@@ -2,7 +2,7 @@ import flask
 from flask import render_template, request, redirect, flash, url_for
 
 from catalog.create import create
-from catalog.create.forms import CreateItemForm
+from catalog.create.forms import CreateCategoryForm, CreateItemForm
 from catalog.database import Session
 from catalog.models import Category, Item, User
 
@@ -11,18 +11,53 @@ def get_category_by_slug(category_slug):
     return Session.query(Category).filter(Category.slug == category_slug).one_or_none()
 
 
+def get_user_by_name(user_name):
+    return Session.query(User).filter(User.name == user_name).one_or_none()
+
+
 @create.route('/catalog/new', methods=['GET', 'POST'])
-@create.route('/catalog/<string:category_slug>/new', methods=['GET', 'POST'])
-def create_item(category_slug=None):
+def create_category():
     if not flask.session['logged_in']:
         flash('Must be logged in to create a new item.', 'warning')
         return redirect(url_for('read.index'))
 
-    form = CreateItemForm()
+    form = CreateCategoryForm(request.form)
+    categories = Session.query(Category).all()
 
-    if request.method == 'POST':
-        return 'Form posted.'
-
+    if request.method == 'POST' and form.category_submit():
+        category = Category(form.new_category.data or form.categories.data)
+        if category.name not in map(lambda x: x.name, categories):
+            Session.add(category)
+            Session.commit()
+        flash('Category selected', 'info')
+        return redirect(url_for('create.create_item', category_slug=category.slug))
     elif request.method == 'GET':
-        category = get_category_by_slug(category_slug)
+        return render_template('create_item.html', form=form, category=None, categories=categories)
+
+
+@create.route('/catalog/<string:category_slug>/new', methods=['GET', 'POST'])
+def create_item(category_slug):
+    if not flask.session['logged_in']:
+        flash('Must be logged in to create a new item.', 'warning')
+        return redirect(url_for('read.index'))
+
+    print('w/ slug', request.referrer)
+    print(category_slug)
+    category = get_category_by_slug(category_slug)
+    form = CreateItemForm(request.form)
+    print(category.slug)
+
+    if request.method == 'POST' and form.validate():
+        item = Item(
+            name=form.name.data,
+            description=form.description.data,
+            user=get_user_by_name(flask.session['user']['name']),
+            category=category)
+        print(item)
+        Session.add(item)
+        Session.commit()
+        flash('New item created.', 'info')
+
+        return redirect(url_for('read.read_item', category_slug=category.slug, item_slug=item.slug))
+    elif request.method == 'GET':
         return render_template('create_item.html', form=form, category=category)
