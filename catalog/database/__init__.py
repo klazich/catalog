f8b6902cb8e4a8e8526c624e7d0dc82554d5688c
Kevin Lazich <dev@kevinlazich.com>
2018-01-21 22:46:01 -0500

    Redo database populating function

--- catalog/database/__init__.py
+++ catalog/database/__init__.py
@@ -1,7 +1,10 @@
+import random
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from mimesis import Generic
 
-from catalog.models import metadata
+from catalog.models import metadata, Item, Category, User
 from config import config_obj
 
 engine = create_engine(config_obj['default'].DATABASE_URI)
@@ -9,3 +12,104 @@ metadata.create_all(bind=engine)
 
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 session = scoped_session(Session)
+
+g = Generic('en')
+
+
+class RandomItemCategoriesUser:
+    def __init__(self):
+        self._mimesis_category_funcs = [
+            g.address.city, g.address.state, g.business.company, g.development.programming_language, g.food.dish,
+            g.food.drink, g.games.game, g.hardware.cpu, g.internet.emoji, g.internet.home_page, g.internet.subreddit,
+            g.science.chemical_element, g.transport.car, g.unit_system.unit]
+        self._used_item_names = set()
+        self._used_user_email = set()
+
+    def item(self):
+        func = random.choice(self._mimesis_category_funcs)
+        category_name = func.__name__
+        item_name = func()
+        if item_name in self._used_item_names:
+            category_name, item_name = self.item()
+        self._used_item_names.add(item_name)
+        return category_name, item_name
+
+    def user(self):
+        user_email = g.personal.email()
+        user_name = g.personal.name()
+        if user_email in self._used_user_email:
+            user_email, user_name = self.user()
+        self._used_user_email.add(user_email)
+        return user_email, user_name
+
+    def categories(self):
+        return [func.__name__ for func in self._mimesis_category_funcs]
+
+
+def init_db():
+    print('creating tables from metadata...', end='')
+    metadata.create_all(bind=engine)
+    print('done')
+
+
+def drop_db():
+    print('cropping tables from metadata...', end='')
+    metadata.drop_all(bind=engine)
+    print('done')
+
+
+def populate_db():
+    drop_db()
+    init_db()
+    _populate_users()
+    _populate_categories()
+    _populate_items()
+
+
+def _populate_users(n=100):
+    print('populating users table...', end='')
+    p = RandomItemCategoriesUser()
+    users = []
+    while len(users) < n:
+        user_email, user_name = p.user()
+        user_picture = g.personal.avatar()
+        users.append(User(
+            name=user_name,
+            email=user_email,
+            picture=user_picture))
+    session.add_all(users)
+    session.commit()
+    print('done')
+
+
+def _populate_categories():
+    print('populating categories table...', end='')
+    p = RandomItemCategoriesUser()
+    categories = []
+    for category_name in p.categories():
+        categories.append(Category(
+            name=category_name.replace('_', ' ')))
+    session.add_all(categories)
+    session.commit()
+    print('done')
+
+
+def _populate_items(n=600):
+    print('populating items table...', end='')
+    p = RandomItemCategoriesUser()
+    users = session.query(User).all()
+    categories = {c.name: c for c in session.query(Category).all()}
+    items = []
+    while len(items) < n:
+        item_category_name, item_name = p.item()
+        item_description = g.text.text()
+        item_user = random.choice(users)
+        item_category = categories[item_category_name.replace('_', ' ')]
+        items.append(Item(
+            name=item_name,
+            description=item_description,
+            category=item_category,
+            user=item_user))
+    session.add_all(items)
+    session.commit()
+    print('done')
