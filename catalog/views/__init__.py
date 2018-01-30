60c66a694b157d39846635931fc0e3961afbf650
Kevin Lazich <dev@kevinlazich.com>
2018-01-30 08:20:54 -0500

    Update catalog/views/__init__.py with commenting

--- catalog/views/__init__.py
+++ catalog/views/__init__.py
@@ -5,9 +5,21 @@ from flask import flash, redirect, request, url_for
 from requests_oauthlib import OAuth2Session
 from requests_oauthlib.compliance_fixes import facebook_compliance_fix
 
+from config import GoogleAuthConfig, FacebookAuthConfig, BaseConfig
+
 
 def get_oauth2_session(config, state=None, token=None):
-    """Creates a OAuth2 session from config specs."""
+    """
+
+    :param config: Google or Facebook config object from config.py
+    :type config: Union[GoogleAuthConfig, FacebookAuthConfig]
+    :param state: the state string
+    :type state: str
+    :param token: the token object
+    :type token: object
+    :return: a OAuth2 session initialized with parameters from config
+    :rtype: OAuth2Session
+    """
     oauth2_session = OAuth2Session(
         client_id=config.CLIENT_ID,
         scope=config.SCOPE,
