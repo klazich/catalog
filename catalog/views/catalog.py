180bd17916ef44adc9fdbb46a938dd1ed7f4c030
Kevin <kevin.lazich@gmail.com>
2018-01-31 09:54:41 -0500

    Update catalog.py with commenting

--- catalog/views/catalog.py
+++ catalog/views/catalog.py
@@ -9,5 +9,9 @@ catalog_bp = Blueprint('catalog', __name__)
 @catalog_bp.route('/')
 @catalog_bp.route('/catalog/')
 def index():
+    """
+    App html entry point. Renders the homepage which list all categories in the database.
+    :return: rendering of the read_catalog.html template
+    """
     g.categories = sorted(session.query(Category).all(), key=lambda c: c.name)
     return render_template('read_catalog.html')
